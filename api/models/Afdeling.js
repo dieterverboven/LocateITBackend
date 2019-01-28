53428f975db0e51dc77073ff686f5c95245f24f7
dieterverboven <dieter.verboven@outlook.com>
2019-01-28 11:14:11 +0100

    afdeling schema

--- api/models/Afdeling.js
+++ api/models/Afdeling.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var afdelingSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    naam: String,
+    naam: String
 })
 
 module.exports = mongoose.model('Afdeling', afdelingSchema);
