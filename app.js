92829c20620111ba5ca3146ec6cb537d3c0732ba
Cel Pynenborg <celpynenborg@gmail.com>
2019-01-28 11:08:49 +0100

    added routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(cors());
 
 const feedbackRoutes = require('./api/routes/Feedbacks');
 const productRoutes = require('./api/routes/Products');
+const afdelingRoutes = require('./api/routes/Afdelingen');
 
 
 mongoose.connect('mongodb://dieterverboven:admin@locateit-shard-00-00-8tyvx.mongodb.net:27017,locateit-shard-00-01-8tyvx.mongodb.net:27017,locateit-shard-00-02-8tyvx.mongodb.net:27017/test?ssl=true&replicaSet=LocateIT-shard-0&authSource=admin&retryWrites=true', { useMongoClient: true });
@@ -17,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/Feedback', feedbackRoutes);
 app.use('/Products', productRoutes);
-
+app.use('/Afdelingen', afdelingRoutes);
 
 app.get('/', (req, res)=> {
    res.send('Hello World');
