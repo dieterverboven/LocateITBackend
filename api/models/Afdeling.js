d71cb9c991d87b528655f3222a548466a85398f6
Cel Pynenborg <celpynenborg@gmail.com>
2019-01-28 11:05:35 +0100

    Afdelingen

---
+++ api/models/Afdeling.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var afdelingSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    naam: String,
+})
+
+module.exports = mongoose.model('Afdeling', feedbackSchema);
---
+++ api/routes/Afdelingen.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Afdeling = require('../models/Afdeling');
+
+//get all afdeling
+router.get('/', (req, res, next)=> {
+    Afdeling.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
+});
+
+// add afdeling
+router.post('/', (req, res, next)=> {
+    const afdeling = new Afdeling({
+        _id: new mongoose.Types.ObjectId(),
+        naam: String
+    });
+    afdeling
+    .save()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
+    res.status(201).json({
+        message: 'Klaar',
+        aangemaakteAfdeling: afdeling
+    })
+});
+
+module.exports = router;
