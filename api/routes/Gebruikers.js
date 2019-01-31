76ad185b06a49d81f07f9f4e5e41f5dcbaa454d2
dieterverboven <dieter.verboven@outlook.com>
2019-01-31 14:47:33 +0100

    gebruiker toevoegen

--- api/routes/Gebruikers.js
+++ api/routes/Gebruikers.js
@@ -5,4 +5,67 @@ const mongoose = require('mongoose');
 const Gebruiker = require('../models/Gebruiker');
 const Config = require('../../config');
 
+// add gebruiker
+router.post('/', (req, res, next)=> {
+    if(req.headers.token == Config.secret){
+        Gebruiker.find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if (user.length >= 1)
+            {
+                return res.status(409).json({
+                    message: "Mail is al gebruikt!"
+                });
+            }
+            else
+            {
+                const gebruiker = new Gebruiker({
+                    _id: new mongoose.Types.ObjectId(),
+                    naam: req.body.naam,
+                    niveau: Number,
+                    email: String,
+                    passwoord: String,
+                });
+                gebruiker
+                .save()
+                .then(result => {
+                    console.log(result);
+                })
+                .catch(err => console.log(err));
+                res.status(201).json({
+                    message: 'Klaar',
+                    aangemaakteGebruiker: gebruiker
+                });
+            }
+        })     
+    } else {
+        res.status(401).json({
+            'reason':'unauthorized'
+        });
+    }
+});
+
+router.post('/login', (req, res, next)=> {
+    if(req.headers.token == Config.secret){
+        console.log(req.body);
+        Gebruiker.findOne({email: req.body.email, passwoord: req.body.passwoord})
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+    } else {
+        res.status(401).json({
+            'reason':'unauthorized'
+        });
+    }
+    
+});
+
 module.exports = router;
