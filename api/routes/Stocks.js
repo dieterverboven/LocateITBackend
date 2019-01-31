75eb3608df3d7b5b19ea3482c7c4ac8a18bb4d0d
dieterverboven <dieter.verboven@outlook.com>
2019-01-31 13:31:28 +0100

    stocks auth token

--- api/routes/Stocks.js
+++ api/routes/Stocks.js
@@ -3,47 +3,64 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Stock = require('../models/Stock');
+const Config = require('../../config');
 
 //get lastest stock
+
 router.get('/', (req, res, next)=> {
-    Stock.findOne({}, 
-        {}, 
-        { 
-            sort: { 'timestamp' : -1 } 
-        }, 
-        function(err, post) {
-            console.log( post );
+    if(req.headers.token == Config.secret){
+        Stock.findOne({}, 
+            {}, 
+            { 
+                sort: { 'timestamp' : -1 } 
+            }, 
+            function(err, post) {
+                console.log( post );
+            })
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
         })
-    .exec()
-    .then(docs => {
-        console.log(docs);
-        res.status(200).json(docs);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        })
-    });
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+    } else {
+        res.status(401).json({
+            'reason':'unauthorized'
+        });
+    }
 });
 
 // add afdeling
 router.post('/', (req, res, next)=> {
-    const stock = new Stock({
-        _id: new mongoose.Types.ObjectId(),
-        inStock: req.body.inStock,
-        timestamp: req.body.timestamp
-    });
-    stock
-    .save()
-    .then(result => {
-        console.log(result);
-    })
-    .catch(err => console.log(err));
-    res.status(201).json({
-        message: 'Klaar',
-        aangemaakteStock: stock
-    })
+    if(req.headers.token == Config.secret){
+
+        const stock = new Stock({
+            _id: new mongoose.Types.ObjectId(),
+            inStock: req.body.inStock,
+            timestamp: req.body.timestamp
+        });
+        stock
+        .save()
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => console.log(err));
+        res.status(201).json({
+            message: 'Klaar',
+            aangemaakteStock: stock
+        })
+
+    } else {
+        res.status(401).json({
+            'reason':'unauthorized'
+        });
+    }
+    
 });
 
 module.exports = router;
